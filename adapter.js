9c956041d1dd6eb8c0b9b52e635999657b64766b
Ryan Bell <rjb1928@rit.edu>
2016-12-24 05:13:01 +0000

    pull input and output from args instead of hard-coded

--- adapter.js
+++ adapter.js
@@ -1,10 +1,8 @@
 const ffmpeg        = require('ffmpeg.js');
 const fs            = require('fs');
 
-//TODO: the name actually needs to come from the args
-var bufferData = new Uint8Array(fs.readFileSync('test.webm'));
-var result = ffmpeg({
-	MEMFS    : [{ name: 'test.webm', data: bufferData }],
+//base options
+let ffmpegOptions = {
 	arguments: process.argv.slice(2), 
 	print    : function(data){
 		console.log(data);
@@ -12,8 +10,20 @@ var result = ffmpeg({
 	printErr : function(data){
 		console.error(data);
 	}
-});
+};
 
+//file i/o?
+var inputFlagIndex = process.argv.indexOf('-i'); 
+if(inputFlagIndex > 0 && process.argv.length >= inputFlagIndex + 1){
+	let input = process.argv[inputFlagIndex + 1];
+	let bufferData = new Uint8Array(fs.readFileSync(input));
+	ffmpegOptions.MEMFS = [{ name: input, data: bufferData }];
+}
+
+//run ffmepg
+var result = ffmpeg(ffmpegOptions);
+
+//write result to disk if there was a file io operation
 var out = result.MEMFS[0];
 if(out){
 	fs.writeFileSync(out.name, Buffer(out.data));
